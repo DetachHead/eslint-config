d7b493734bbb3e05d6b73af78af9501f67947943
detachhead <detachhead@users.noreply.github.com>
2021-11-19 16:49:58 +1000

    disable `import/export` as typescript handles it

--- index.ts
+++ index.ts
@@ -49,6 +49,7 @@ const eslint: Linter.Config = {
             { devDependencies: ['**/*.test.ts', '**/*.spec.ts', 'jest.config.ts'] },
         ],
         'import/no-unresolved': 'off', // false positives, typescript handles this anyway
+        'import/export': 'off', // false positives when default exporting a class and an interface, typescript checks this anyway
         'import/no-duplicates': 'error',
         'require-unicode-regexp': 'error',
         'no-trailing-spaces': 'error',
